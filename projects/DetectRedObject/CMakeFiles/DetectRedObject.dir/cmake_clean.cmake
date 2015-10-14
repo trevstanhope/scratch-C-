0b48ed7302dd81895b1ed268e589bec17ad330a4
Trevor Stanhope <trevor@think>
2015-10-14 18:29:20 -0400

    added red detect demo

---
+++ projects/DetectRedObject/CMakeFiles/DetectRedObject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/DetectRedObject.dir/DetectRedObject.cpp.o"
+  "DetectRedObject.pdb"
+  "DetectRedObject"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/DetectRedObject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
