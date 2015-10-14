c5be9f9804578aed4cc52d0aec61c519b1079cd3
Trevor Stanhope <trevor@think>
2015-10-14 18:10:41 -0400

    added surf demo

---
+++ projects/SURFKeypointMatching/CMakeFiles/SURFKeypointMatching.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/SURFKeypointMatching.dir/SURFKeypointMatching.cpp.o"
+  "SURFKeypointMatching.pdb"
+  "SURFKeypointMatching"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/SURFKeypointMatching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
