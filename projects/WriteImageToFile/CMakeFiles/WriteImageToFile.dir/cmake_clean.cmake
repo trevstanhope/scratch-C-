70d0d83ff483b2ba099c789206c4a607b31cd90e
Trevor Stanhope <trevor@think>
2015-10-14 17:53:30 -0400

    some tutorials

---
+++ projects/WriteImageToFile/CMakeFiles/WriteImageToFile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/WriteImageToFile.dir/WriteImageToFile.cpp.o"
+  "WriteImageToFile.pdb"
+  "WriteImageToFile"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/WriteImageToFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ projects/WriteImageToFile/CMakeFiles/WriteToFile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/WriteToFile.dir/WriteToFile.cpp.o"
+  "WriteToFile.pdb"
+  "WriteToFile"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/WriteToFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
